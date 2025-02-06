50f31a37b1f7bfe109c8c5defeb1304e1f87b2ce
Jennypennylenny <j.graf2019@icloud.com>
2025-02-06 14:39:01 +0100

    Payments hinzugefügt

---
+++ DB_ECommerce.MVC/ViewModels/Payments/PaymentCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DB_ECommerce.MVC.ViewModels.Payments
+{
+    public class PaymentCreateViewModel
+    {
+        public string Currency { get; set; }
+        public string PaymentMethod { get; set; }
+        public string PaymentStatus { get; set; }
+        public decimal PaymentAmount { get; set; }
+        public decimal OpenPayment { get; set; }
+    }
+}
